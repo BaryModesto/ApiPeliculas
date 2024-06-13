97f042bf91d39e4bbed830a7c8ee7df7c2870540
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-13 20:06:48 -0300

    feat: ICategoriaRepositorio

---
+++ Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using ApiPeliculas.Modelos;
+
+namespace ApiPeliculas.Repositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        ICollection<Categoria> Get_Categorias();
+        Categoria GetCategoria(int _categoria);
+        bool Existe_Categorias(string _nombre);
+        bool Crear_Categoria(Categoria _categoria);
+        bool Actualizar_Categorias(Categoria _categoria);
+        bool Borrar_Categoria(Categoria _caategoria);
+        bool Guardar();
+    }
+}
