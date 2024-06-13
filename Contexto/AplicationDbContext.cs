0c32435f6f924809ee2ef2d95316c1e935c74c8c
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-13 12:12:15 -0300

    feat: Migracion creada en la base de datos

--- Contexto/AplicationDbContext.cs
+++ Contexto/AplicationDbContext.cs
@@ -7,6 +7,6 @@ namespace ApiPeliculas.Migraciones
     {
         public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options){}
 
-        public DbSet<Categoria> Categorias {  get; set; }
+        public DbSet<Categoria> Categoria {  get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddDbContext<AplicationDbContext>(opciones =>
+//{
+//    opciones.UseSqlServer(builder.Configuration.GetConnectionString("Cadena_Coneccion"));
+//});
 // Add services to the container.
 
 builder.Services.AddControllers();
