04dbe8ed9d9cc4bfeb82a601668cb8678445f4a8
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-13 11:25:28 -0300

    feat: configurar el DbContext

---
+++ Migraciones/AplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiPeliculas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Migraciones
+{
+    public class AplicationDbContext:DbContext 
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options){}
+
+        public DbSet<Categoria> Categorias {  get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using ApiPeliculas.Migraciones;
+using ApiPeliculas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Cadena_Coneccion")));
 
 var app = builder.Build();
 
