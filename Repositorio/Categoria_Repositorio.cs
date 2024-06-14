625e93fa1d2b8670060d694a4893f0ac15cf4b83
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-13 21:04:40 -0300

    feat: CsategortiaRepositorio

---
+++ Repositorio/Categoria_Repositorio.cs
@@ -0,0 +1,72 @@
+﻿using ApiPeliculas.Migraciones;
+using ApiPeliculas.Modelos;
+using ApiPeliculas.Repositorio.IRepositorio;
+
+namespace ApiPeliculas.Repositorio
+{
+    public class Categoria_Repositorio : ICategoriaRepositorio
+    {
+        AplicationDbContext bd;
+        public Categoria_Repositorio(AplicationDbContext _bd)
+        {
+            bd = _bd;
+        }
+        public bool Actualizar_Categorias(Categoria _categoria)
+        {
+            _categoria.FechaCreacion = DateTime.Now;
+            bd.Update(_categoria);
+            return Guardar();
+        }
+
+        public bool Borrar_Categoria(Categoria _categoria)
+        {
+            bd.Remove(_categoria);
+            return Guardar();
+        }
+
+        public bool Crear_Categoria(Categoria _categoria)
+        {
+            _categoria.FechaCreacion = DateTime.Now;
+            bd.Categoria.Add(_categoria);
+            return Guardar();
+        }
+
+        public bool Existe_Categorias(string _nombre)
+        {
+            bool encomtrado = false;
+            try
+            {
+                encomtrado = bd.Categoria.Any(x => x.Nombre.ToLower().Trim() == _nombre.ToLower().Trim());
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return encomtrado;
+        }
+        public bool Existe_Categorias(int _id)
+        {
+            if ( bd.Categoria.Find(_id) == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public Categoria GetCategoria(int _categoria)
+        {
+            Categoria cat = bd.Categoria.Find(_categoria);
+            return cat;
+        }
+
+        public ICollection<Categoria> Get_Categorias()
+        {
+            return bd.Categoria.OrderBy(x =>  x.Nombre).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return (bd.SaveChanges() >0)? true:false;
+        }
+    }
+}
--- Repositorio/IRepositorio/ICategoriaRepositorio.cs
+++ Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -7,6 +7,7 @@ namespace ApiPeliculas.Repositorio.IRepositorio
         ICollection<Categoria> Get_Categorias();
         Categoria GetCategoria(int _categoria);
         bool Existe_Categorias(string _nombre);
+        bool Existe_Categorias(int _id);
         bool Crear_Categoria(Categoria _categoria);
         bool Actualizar_Categorias(Categoria _categoria);
         bool Borrar_Categoria(Categoria _caategoria);
