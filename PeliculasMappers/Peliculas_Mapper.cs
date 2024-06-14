f738a90e2e8cd8be9708c1a3be2d8e30e06884e4
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-13 21:24:36 -0300

    feat: Automapper

---
+++ PeliculasMappers/Peliculas_Mapper.cs
@@ -0,0 +1,15 @@
+﻿using ApiPeliculas.Modelos;
+using ApiPeliculas.Modelos.Dtos;
+using AutoMapper;
+
+namespace ApiPeliculas.PeliculasMapper
+{
+    public class Peliculas_Mapper: Profile
+    {
+        public Peliculas_Mapper()
+        {
+            CreateMap<Categoria, Categoria_Dto>().ReverseMap();
+            CreateMap<Categoria, Crear_Categoria_Dto>().ReverseMap();
+        }
+    }
+}
