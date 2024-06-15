eee1a42e8e91db5f89d3ecf1af0ed8c01394aec9
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-15 19:39:24 -0300

    feat: Registro de Usuario

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -4,6 +4,7 @@ using ApiPeliculas.Repositorio.IRepositorio;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ApiPeliculas.Controllers
 {
@@ -13,10 +14,12 @@ namespace ApiPeliculas.Controllers
     {
         IUsuarioRepositorio user_repo;
         IMapper mapper;
+        protected RespiuestaAPI respuesta_api;
         public UsuariosController(IUsuarioRepositorio _user_repo,IMapper _mapper)
         {
             user_repo = _user_repo;
             mapper = _mapper;
+            this.respuesta_api = new();
         }
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -31,6 +34,49 @@ namespace ApiPeliculas.Controllers
                 list_dto.Add(mapper.Map<UsuarioDto>(i));
             }
             return Ok(list_dto);
+        }
+        [HttpGet("{usuarioId:int}", Name = "Coger_Usuario")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get_Usuario(int usuarioId)
+        {
+            var especifico = user_repo.GetUsuario(usuarioId);
+            if (especifico == null)
+            {
+                return NotFound();
+            }
+            var u_dto = mapper.Map<UsuarioDto>(especifico);
+            return Ok(u_dto);
+        }
+        [HttpPost("Registro")]
+        [ProducesResponseType(201, Type = typeof(UsuarioRegistroDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Crear_Categoria([FromBody] UsuarioRegistroDto _user_dto)
+        {
+            bool validar_nombre_usuario = user_repo.ES_Unico_Usuario(_user_dto.NombreUsuario);
+            if (!validar_nombre_usuario)
+            {
+                respuesta_api.StatusCode = HttpStatusCode.BadRequest;
+                respuesta_api.IsSucess = false;
+                respuesta_api.ErrorMesasages.Add("El nombre d4e usuario ya existe");
+                return BadRequest(respuesta_api);
+            }
+            var usuario = await user_repo.Registro(_user_dto);
+            if (usuario == null)
+            {
+                respuesta_api.StatusCode = HttpStatusCode.BadRequest;
+                respuesta_api.IsSucess = false;
+                respuesta_api.ErrorMesasages.Add("El nombre d4e usuario ya existe");
+                return BadRequest(respuesta_api);
+            }
+
+            respuesta_api.StatusCode = HttpStatusCode.OK;
+            respuesta_api.IsSucess = true;            
+            return Ok(respuesta_api);
         }
     }
 }
