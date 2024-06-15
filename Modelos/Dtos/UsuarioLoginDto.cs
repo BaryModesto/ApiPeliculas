1f5e43104c412d7a543f5e7fae5e524d2c005d88
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-15 17:04:17 -0300

    feat:Login

--- Modelos/Dtos/UsuarioLoginDto.cs
+++ Modelos/Dtos/UsuarioLoginDto.cs
@@ -5,7 +5,7 @@ namespace ApiPeliculas.Modelos.Dtos
     public class UsuarioLoginDto
     {
         [Required(ErrorMessage ="El usuario es obligatorio")]
-        public int NombreUsuario { get; set; }
+        public string NombreUsuario { get; set; }
       
         [Required(ErrorMessage ="El password es obligatorio")]
         public string Password { get; set; }
--- Repositorio/UsuarioRepositorio.cs
+++ Repositorio/UsuarioRepositorio.cs
@@ -2,6 +2,10 @@
 using ApiPeliculas.Modelos;
 using ApiPeliculas.Modelos.Dtos;
 using ApiPeliculas.Repositorio.IRepositorio;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using XSystem.Security.Cryptography;
 
 namespace ApiPeliculas.Repositorio
@@ -9,9 +13,11 @@ namespace ApiPeliculas.Repositorio
     public class UsuarioRepositorio : IUsuarioRepositorio
     {
         AplicationDbContext bd;
-        public UsuarioRepositorio(AplicationDbContext _bd)
+        string palabra_secreta;
+        public UsuarioRepositorio(AplicationDbContext _bd,IConfiguration _conf)
         {
             bd = _bd;
+            palabra_secreta = _conf.GetValue("Api_Settings", "Palabra_Secreta");// 
         }
 
         public bool ES_Unico_Usuario(string _nombre_usuario)
@@ -33,9 +39,39 @@ namespace ApiPeliculas.Repositorio
             return bd.Usuario.OrderBy(x => x.NombreUsuario).ToList();
         }
 
-        public Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto _user)
+        public async Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto _user)
         {
-            throw new NotImplementedException();
+            var password_encriptado = obtenermd5(_user.Password);
+            var usuario = bd.Usuario.FirstOrDefault(x => x.NombreUsuario.ToLower() == _user.NombreUsuario.ToLower() && x.Password == password_encriptado);
+            if (usuario == null)
+            {
+                UsuarioLoginRespuestaDto respuesta = new UsuarioLoginRespuestaDto()
+                {
+                    Usuario = null,
+                    Token = ""
+                };
+                return respuesta;
+            }
+            var manejador_token = new JwtSecurityTokenHandler();
+            var Key = Encoding.ASCII.GetBytes(palabra_secreta);
+            var token_descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.NombreUsuario.ToString()),
+                    new Claim(ClaimTypes.Role, usuario.Role)
+                }),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(Key),SecurityAlgorithms.HmacSha256Signature)
+            };
+            //---
+            var token = manejador_token.CreateToken(token_descriptor);
+            UsuarioLoginRespuestaDto usuario_login = new UsuarioLoginRespuestaDto()
+            {
+                Token = manejador_token.WriteToken(token),
+                Usuario = usuario
+            };
+            return usuario_login;
         }
 
         public async Task<Usuario> Registro(UsuarioRegistroDto _user)
