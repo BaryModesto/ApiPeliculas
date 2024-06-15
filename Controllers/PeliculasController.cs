e49f74feffaae8fc2493996957a5a701ae5d6bb9
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-15 08:26:07 -0300

    feat: controlador de Pelicula

---
+++ Controllers/PeliculasController.cs
@@ -0,0 +1,20 @@
+﻿using ApiPeliculas.Repositorio.IRepositorio;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/peliculas")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        IPeliculaRepositorio peli_repo;
+        IMapper mapper;
+        public PeliculasController(IPeliculaRepositorio _peli_repo,IMapper _mapper)
+        {
+            peli_repo = _peli_repo;
+            mapper = _mapper;
+        }
+    }
+}
