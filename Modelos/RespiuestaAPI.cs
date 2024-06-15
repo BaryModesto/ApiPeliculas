028eb53e477127566d2c641f3ad2d51de564c8d3
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-15 19:26:51 -0300

    feat: RespuestaApi

---
+++ Modelos/RespiuestaAPI.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace ApiPeliculas.Modelos
+{
+    public class RespiuestaAPI
+    {
+        public RespiuestaAPI()
+        {
+            ErrorMesasages = new List<string>();
+        }
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSucess {  get; set; }
+        public List<string> ErrorMesasages { get; set; }
+        public object Result { get; set; }
+    }
+}
