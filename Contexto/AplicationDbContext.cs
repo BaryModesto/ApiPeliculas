9aca18312bcef82f88606d824ccb8ba7af09338b
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-23 08:56:54 -0300

    feat: migration de Identity

--- Contexto/AplicationDbContext.cs
+++ Contexto/AplicationDbContext.cs
@@ -1,14 +1,22 @@
 ﻿using ApiPeliculas.Modelos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ApiPeliculas.Migraciones
 {
-    public class AplicationDbContext:DbContext 
+    public class AplicationDbContext:IdentityDbContext<AppUsuario>
     {
         public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
 
         public DbSet<Categoria> Categoria {  get; set; }
         public DbSet<Pelicula> Pelicula { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<AppUsuario> AppUsuario { get; set; }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using ApiPeliculas.PeliculasMapper;
 using ApiPeliculas.Repositorio;
 using ApiPeliculas.Repositorio.IRepositorio;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,13 +18,14 @@ builder.Services.AddAutoMapper(typeof(Peliculas_Mapper));
 
 builder.Services.AddControllers(x =>
 {
-    x.CacheProfiles.Add("PorDefecto30", new CacheProfile { Duration = 30 }); 1
+    x.CacheProfiles.Add("PorDefecto30", new CacheProfile { Duration = 30 }); 
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Cadena_Coneccion")));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AplicationDbContext>();
  builder.Services.AddCors(x => x.AddPolicy("PolicyCors",builder =>
  {
      builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
